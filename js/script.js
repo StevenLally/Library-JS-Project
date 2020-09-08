c1356334d938e6996fccc2ef36c66b8150ff8608
Steven Lally <steve.lally89@gmail.com>
2020-09-08 19:07:03 -0400

    fixing merge error

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 let library = [];
 
-function Book() {
-  //Book constructor
+//Book constructor
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = false;
+  this.toggleRead = () => this.read = !this.read;
 }
 
+function displayLibrary() {
+  const libraryDiv = document.getElementById("library-display");
+  //update div with library array content...maybe better to run this only on load then append additions?
+}
+
+//push new Book to library array based on form input
 function addBookToLibrary() {
-  //push new Book to library array based on form input
+  // @ts-ignore
+  const formInput = document.getElementById("new-book-form").elements;
+
+  const title = formInput[0].value;
+  const author = formInput[1].value;
+  const pages = formInput[2].value;
+
+  library.push(new Book(title, author, pages));
 }
